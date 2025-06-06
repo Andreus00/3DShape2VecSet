f603852a3823c9d9b7f31c10d6cf148592b70105
Andreus00 <sanchiettiandrea@gmail.com>
2025-06-06 15:42:27 +0200

    minor changes

--- start_remote_vs.sh
+++ start_remote_vs.sh
@@ -7,6 +7,10 @@
 #SBATCH --gres=gpu:2
 #SBATCH --mem=32G    # Request more memory
 #SBATCH --cpus-per-task=8   # Request more CPUs
+#SBATCH --mail-type=ALL            # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=andrea.sanchietti@uni-tuebingen.de   # Email to which notifications will be sent
+#SBATCH --output=./vslogs/myjob-%j.out       # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=./vslogs/myjob-%j.err        # File to which STDERR will be written - make sure this is not on $HOME
 
 
 /usr/sbin/sshd -D -p 7123 -f /dev/null -h ${HOME}/.ssh/id_ecdsa
