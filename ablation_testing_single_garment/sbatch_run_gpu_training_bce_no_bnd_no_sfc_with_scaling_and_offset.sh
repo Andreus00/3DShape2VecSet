95138fbcd146ba0a8349f43188f45718f36a87e7
Andreus00 <sanchiettiandrea@gmail.com>
2025-06-23 18:31:30 +0200

    modified test scripts

---
+++ ablation_testing_single_garment/sbatch_run_gpu_training_bce_no_bnd_no_sfc_with_scaling_and_offset.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Sample Slurm job script for Galvani 
+
+#SBATCH -J ge-bce-no-bnd-sfc       # Job name
+#SBATCH --ntasks=1                 # Number of tasks
+#SBATCH --cpus-per-task=8          # Number of CPU cores per task
+#SBATCH --nodes=1                  # Ensure that all cores are on the same machine with nodes=1
+#SBATCH --partition=a100-galvani   # Which partition will run your job
+#SBATCH --time=3-00:00             # Allowed runtime in D-HH:MM
+#SBATCH --mem=16G                  # Total memory pool for all cores (see also --mem-per-cpu); exceeding this number will cause your job to fail.
+#SBATCH --gres=gpu:4
+#SBATCH --output=./logs_ge_bce_no_bnd_no_sfc_scale_offset/myjob-%j.out       # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=./logs_ge_bce_no_bnd_no_sfc_scale_offset/myjob-%j.err        # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mail-type=ALL            # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=andrea.sanchietti@uni-tuebingen.de   # Email to which notifications will be sent
+
+# Diagnostic and Analysis Phase - please leave these in.
+scontrol show job $SLURM_JOB_ID
+pwd
+
+# Setup Phase
+# add possibly other setup code here, e.g.
+# - copy singularity images or datasets to local on-compute-node storage like /scratch_local
+# - loads virtual envs, like with anaconda
+# - set environment variables
+# - determine commandline arguments for `srun` calls
+source ~/.bashrc
+conda activate shape2vec
+# Compute Phase
+# srun env -u SLURM_PROCID python3 main_ae_garmentcode.py --data_path ../GarmentCode/garmentcodedata_v2 --force_occupancy --only_udf # srun will automatically pickup the configuration defined via `#SBATCH` and `sbatch` command line arguments  
+srun env -u SLURM_PROCID python3 -m torch.distributed.launch \
+    --rdzv_endpoint=localhost:29399 main_ae_garmentcode.py \
+    --data_path ../GarmentCode/garmentcodedata_v2 \
+    --device cuda \
+    --batch_size 2 \
+    --accum_iter 32 \
+    --max_dist 0.1 \
+    --warmup_epoch 0 \
+    --lr 0.00001 \
+    --save_every 1 \
+    --epochs 800 \
+    --output_dir output_bce_no_bnd_no_sfc_scaling_offset \
+    --log_dir output_bce_no_bnd_no_sfc_scaling_offset \
+    --point_cloud_size 8192 \
+    --num_samples 32768 \
+    --latent_vec_size 2048 \
+    --surface_samples_ratio 0.0 \
+    --random_samples_ratio 0.5 \
+    --surf_bnd_percent 0.0 \
+    --surf_imp_percent 0.5 \
+    --single_garment_overfit \
+    --grad_weight 0.001 \
+    --kl_weight 0.0001 \
+    --global_scaling 3 \
+    --global_offset_x 1.0 \
+    --global_offset_y 1.0 \
+    --global_offset_z 1.0 \
+
+conda deactivate
